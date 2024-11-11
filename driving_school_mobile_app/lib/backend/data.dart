a8b8e8fc477e2949dca84254de4e399d4cc3d833
RonaldSiyangoma <162575906+RonaldSiyangoma@users.noreply.github.com>
2024-11-11 07:37:03 +0200

    inital app
    inital app

---
+++ driving_school_mobile_app/lib/backend/data.dart
@@ -0,0 +1,44 @@
+List<dynamic> packages = [
+  {
+    'name': 'code 10 with 10 lessons',
+    'price': 3000,
+    'sale_price': 2500,
+    'image': 'assets/images/code10.jpg',
+    'description': 'This package includes 10 lessons and a code 10 license',
+  },
+  {
+    'name': 'code 8 with 10 lessons',
+    'price': 3000,
+    'sale_price': 2500,
+    'image': 'assets/images/code8.jpg',
+    'description': 'This package includes 10 lessons and a code 8 license',
+  },
+  {
+    'name': 'code 14 with 10 lessons',
+    'price': 3000,
+    'sale_price': 2500,
+    'image': 'assets/images/code14.jpg',
+    'description': 'This package includes 10 lessons and a code 14 license',
+  },
+  {
+    'name': 'code 10 with 20 lessons',
+    'price': 6000,
+    'sale_price': 5000,
+    'image': 'assets/images/code10.jpg',
+    'description': 'This package includes 20 lessons and a code 10 license',
+  },
+  {
+    'name': 'code 8 with 20 lessons',
+    'price': 6000,
+    'sale_price': 5000,
+    'image': 'assets/images/code8.jpg',
+    'description': 'This package includes 20 lessons and a code 8 license',
+  },
+  {
+    'name': 'code 14 with 20 lessons',
+    'price': 6000,
+    'sale_price': 5000,
+    'image': 'assets/images/code14.jpg',
+    'description': 'This package includes 20 lessons and a code 14 license',
+  }
+];
---
+++ driving_school_mobile_app/lib/navigator  menu/nav.dart
@@ -0,0 +1,64 @@
+import 'package:driving_school_mobile_app/colors.dart';
+import 'package:driving_school_mobile_app/pages/dashboard.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/learsons.dart';
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Dashboardpage(),
+    TableEventsExample(),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: customblack,
+        unselectedItemColor: lightgray,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule_outlined),
+            label: 'learsons',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school_outlined),
+            label: 'Courses',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: redcolor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
